f23d2fef86ec78fee90c8344acfb38b08cb96295
Krzysztof Mrówczyński <krzysztof.mrowczynski@microsoft.wsei.edu.pl>
2019-04-14 12:17:41 +0200

    Wer next

--- Cw3.4/Program.cs
+++ Cw3.4/Program.cs
@@ -10,6 +10,28 @@ namespace Cw3._4
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int a, b;
+                string text;
+                Console.Write("Podaj wartosc dzielnej (a): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out a);
+                Console.Write("Podaj wartosc dzielnika (b): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out b);
+
+                Console.WriteLine("Wynik dzielnia: {0} ", a / b);
+
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nigdy cholero niedziel przez zero");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
